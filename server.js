cd64a045b26fa4ee5653650ad529d51b2966579f
nostalgia <supergodmodeo@gmail.com>
2026-04-07 17:12:05 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const app = express();
+
+app.use(express.json());
+
+// Conexão MongoDB
+mongoose.connect("mongodb+srv://supergodmodeo_db_user:QE47pW7qv5IEAPjE@nexusgames.96iuubq.mongodb.net/?appName=nexusgames")
+  .then(() => console.log("Conectado ao MongoDB Atlas"))
+  .catch(err => console.error("Erro na conexão:", err));
+
+// Modelo
+const Usuario = mongoose.model('Usuario', new mongoose.Schema({
+  nome: { type: String, unique: true },
+  senha: String
+}));
+
+// Cadastro
+app.post('/registrar', async (req, res) => {
+  try {
+    const { nome, senha } = req.body;
+    const hash = await bcrypt.hash(senha, 10);
+    const novoUsuario = new Usuario({ nome, senha: hash });
+    await novoUsuario.save();
+    res.send("Conta criada com sucesso!");
+  } catch (err) {
+    res.send("Erro ao criar conta: " + err.message);
+  }
+});
+
+// Login
+app.post('/login', async (req, res) => {
+  try {
+    const { nome, senha } = req.body;
+    const usuario = await Usuario.findOne({ nome });
+    if (!usuario) return res.send("Usuário não encontrado!");
+    const valido = await bcrypt.compare(senha, usuario.senha);
+    if (valido) {
+      res.send("Login realizado com sucesso!");
+    } else {
+      res.send("Senha incorreta!");
+    }
+  } catch (err) {
+    res.send("Erro no login: " + err.message);
+  }
+});
+
+app.listen(3000, () => console.log("Servidor rodando em http://localhost:3000"));
