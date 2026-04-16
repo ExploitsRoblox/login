060643670394a4b9a7a34f78867f3e2032e77323
nostalgia <supergodmodeo@gmail.com>
2026-04-16 17:57:47 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,10 +9,7 @@ const SECRET = process.env.JWT_SECRET || "stevejobs"; // use variável de ambien
 
 app.use(express.json({ limit: '50mb' })); // Aumentar limite de tamanho
 app.use(cors({
-    origin: ["https://exploitsroblox.github.io", "http://localhost:3000", "http://localhost:5500"],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+    origin: "https://exploitsroblox.github.io",
 }));
 
 //MongoDB
